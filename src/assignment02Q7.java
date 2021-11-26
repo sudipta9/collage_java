5ab706f2588bf4f40470a80f250c1125edc2c754
sudipta9 <sudiptapradhan58271@gmail.com>
2021-11-26 19:21:55 +0530

    update

--- src/assignment02Q7.java
+++ src/assignment02Q7.java
@@ -1,11 +1,11 @@
 public class assignment02Q7 {
     public static void main(String[] args) {
-        ThreadRun obj = new ThreadRun();
+        Thread obj = new Thread(new ThreadRun());
         obj.start();
     }
 }
 
-class ThreadRun extends Thread {
+class ThreadRun implements Runnable {
     public void run() {
         System.out.println("Thread using Runnable interface");
     }
